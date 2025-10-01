7faf104e9a9fa9101134d504524b591bc8bcf456
anja324 <oanja324@gmail.com>
2025-10-01 23:25:33 +0200

    Created text file for purchasable gifs, condensed all gif purchase commands into a single function, and adjusted the command list text file accordingly.

--- commands/anja_points_store.py
+++ commands/anja_points_store.py
@@ -25,73 +25,54 @@ async def buy_raincoat(message):
         await message.channel.send(f"You have purchased a raincoat for {cost_of_raincoat} AnjaPoints™️.")
 
 
-async def uwu(message):
+def store_gif_dict_creation():
     """
-    Posts a gif of a weeb uwu and deducts 500 points from the user
+    creates an array of purchasable gif-objects
+
+    :return: store_gif_dict
+    """
+    class PurchasableGif:
+        def __init__(self, name, cost, link, rejection_message):
+            self.name = name
+            self.cost = cost
+            self.link = link
+            self.rejection_message = rejection_message
+
+    store_gif_file = open("text_files/point_store_gifs", "r")
+    store_gif_list = []
+    store_gif_dict = {}
+
+    for line in store_gif_file:
+        gif = line.split(" ", 3)
+        store_gif_list.append(gif)
+
+    for gif in store_gif_list:
+        gif_to_add = PurchasableGif(*gif)
+        store_gif_dict[gif_to_add.name] = gif_to_add
+
+    return store_gif_dict
+
+
+async def gif_purchase(message):
+    """
+    Posts purchased gif, provided user can afford it.
     :param message: The raw user inputted message
     :return:
     """
-
+    #   creates gif dictionary
+    #   retrieves author score information
     user_id = message.author.id
     current_score = await score_query(user_id)
-    if current_score >= 500:
-        amount_to_deduct = 500
+    #   identify desired gif
+    requested_gif = store_gif_dict.get(message.content)
+    #   determine if gif is eligible to be purchased and send gif or rejection message
+    if current_score >= int(requested_gif.cost):
+        amount_to_deduct = requested_gif.cost
         await deduct_from_score(user_id, amount_to_deduct)
-        await message.channel.send("You have spent 500 AnjaPoints™️")
-        await message.channel.send("https://tenor.com/bpHsW.gif")
+        await message.channel.send(f"You have spent {requested_gif.cost} AnjaPoints™️")
+        await message.channel.send(requested_gif.link)
     else:
-        await message.channel.send("You cannot afford to uwu.")
+        await message.channel.send(requested_gif.rejection_message)
 
+store_gif_dict = store_gif_dict_creation()
 
-async def nom(message):
-    """
-    Posts a link containing a cookie monster gif and deducts 50 points from the user.
-    :param message: The raw user inputted message
-    :return:
-    """
-
-    user_id = message.author.id
-    current_score = await score_query(user_id)
-    if current_score >= 50:
-        amount_to_deduct = 50
-        await deduct_from_score(user_id, amount_to_deduct)
-        await message.channel.send("You have spent 50 AnjaPoints™️")
-        await message.channel.send("https://tenor.com/view/cookies-gif-14785632")
-    else:
-        await message.channel.send("You cannot afford to nom.")
-
-
-async def ok_bro(message):
-    """
-    Posts a gif of a sexy man vin likes and deducts 500 points from the user
-    :param message: The raw user inputted message
-    :return:
-    """
-
-    user_id = message.author.id
-    current_score = await score_query(user_id)
-    if current_score >= 500:
-        amount_to_deduct = 500
-        await deduct_from_score(user_id, amount_to_deduct)
-        await message.channel.send("You have spent 500 AnjaPoints™️")
-        await message.channel.send("https://tenor.com/view/ok-bro-gif-23915781")
-    else:
-        await message.channel.send("You cannot afford to ogle those washboard abs.")
-
-
-async def lizard(message):
-    """
-    Posts a gif of a lizard and deducts 500 points from the user
-    :param message: The raw user inputted message
-    :return:
-    """
-
-    user_id = message.author.id
-    current_score = await score_query(user_id)
-    if current_score >= 500:
-        amount_to_deduct = 500
-        await deduct_from_score(user_id, amount_to_deduct)
-        await message.channel.send("You have spent 500 AnjaPoints™️")
-        await message.channel.send("https://tenor.com/view/lizard-lizard-lizard-button-lizard-tom-the-lizard-pixar-gif-17194109322617058326")
-    else:
-        await message.channel.send("You cannot afford to let the lizard spank.")
